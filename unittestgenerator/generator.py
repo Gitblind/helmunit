361c1f6ddbe71a8bcb0a2ab597dd494cf96a385b
Rong Ye <yerong.mobile@gmail.com>
2019-07-04 13:25:36 +1000

    initial workable version

--- unittestgenerator/generator.py
+++ unittestgenerator/generator.py
@@ -1,5 +1,11 @@
 import os,re,shutil
 
+def find_nth(string, substring, n):
+   if (n == 1):
+       return string.find(substring)
+   else:
+       return string.find(substring, find_nth(string, substring, n - 1) + 1)
+
 # Ask for a directory
 path = '.'
 # path = input('Enter directory: ')
@@ -23,35 +29,42 @@ else:
     print("There's no .yaml files in templates directory")
     raise SystemExit
 
-# List all yaml files under template directory
-for filename in yamlfiles:
+# Create _test.yaml file for each template yaml file
+def create_test_file_for(filename):
     try:
-        # Create _test.yaml file for each template yaml file
         testfilename = re.search('templates/(.+?).yaml', filename).group(1)+"_test.yaml"
     except AttributeError:
         print("Error, can not find the yaml file")
     
     print("Create a new test file:" + testpath + testfilename)
     testfile = open(testpath + testfilename, 'w') 
+    return testfile
+
+# List all yaml files under template directory
+for filename in yamlfiles:
+    
+    testfile = create_test_file_for(filename)
 
     #read yaml file line by line
     lines = [line.rstrip('\n') for line in open(filename)]
-    currentlevel = 0
-    prefix = ''
+    parentlevel = 0
     levelspacenumber = 2
+    prefix = ''
+
     for line in lines:
         if (line.find(":") > -1):
-            pathindicator = prefix + line[:line.index(":")]
-            print(pathindicator)
-            # testfile.write(pathindicator + "\n")
-            # prefix = prefix + 
+            line = line.replace("-", " ")
+            level = (len(line)-len(line.lstrip(' ')))/levelspacenumber
 
-            # level = (len(line)-len(line.rstrip(' ')))/2
-            # line = line.rstrip(' ')
-            # level = level
-            
-            # prefix = prefix + levelspace
-        else:
-            prefix = ''
+            if level == 0:
+                pathindicator = line[:line.index(":")].lstrip(' ')
+                prefix = line[:line.index(":")].lstrip(' ') + "."
+            elif level > parentlevel :
+                pathindicator = prefix + line[:line.index(":")].lstrip(' ')
+                prefix = prefix + line[:line.index(":")].lstrip(' ') + "."
+            elif level <= parentlevel :
+                pathindicator = prefix + line[:line.index(":")].lstrip(' ')
+                prefix = prefix[0:find_nth(prefix, ".", level)+1]
 
-
+            parentlevel = level
+            testfile.write(pathindicator + "\n")
